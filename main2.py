3fb80d15cfd6130b841f63ec69f0b2dc8c5e6d50
Mykaella98 <mykaella.sr@outlook.com>
2025-02-20 10:57:18 +0100

    modifica file.txt
    Aggiunto hello world

--- main2.py
+++ main2.py
@@ -1,3 +1,8 @@
-nome = "mika"
+nome = "Mika"
 cognome = "Souza"
 print(nome, cognome)
+
+with open("output.txt", mode = "a") as file:
+    file.write("hello world")
+
+print("\nStringa 'hello word' scritta sul file 'output.txt' corretamente.")
